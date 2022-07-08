38013eee38528fd9dbf5c14cb4ca27f3b7f3e273
Dsimmanuel <dsimmanuel592@gmail.com>
2022-07-08 15:47:07 +0530

    service viewTodos() Subscribed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewTodosComponent } from './view-todos/view-todos.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import { ViewTodosComponent } from './view-todos/view-todos.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
